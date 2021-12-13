bae562a72c34dd4b72d5a58b223ad6c1138e0088
PG <pg@BBMQAPANKAJGUP.local>
2021-12-13 13:31:50 +0530

    fix merge

--- src/main/java/com/example/practice1/ResumeDisplayController.java
+++ src/main/java/com/example/practice1/ResumeDisplayController.java
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
-<<<<<<<< HEAD:src/main/java/com/example/practice1/ResumeDisplayController.java
->>>>>>> 3174df623b76f3e6be593e34217727a0a23c877a
+
 package com.example.practice1;
 
 import org.springframework.stereotype.Controller;
@@ -22,17 +19,10 @@ public class ResumeDisplayController
     public String home()
     {
         System.out.print("Welcome to home page");
-<<<<<<< HEAD
         //return "index.jsp";//Just we have to return the jsp page & return type should be String bcz application.prop .jsp prefix added
         return "displayresume";
     }
 }
-=======
-        //return "index";//Just we have to return the jsp page & return type should be String bcz application.prop .jsp prefix added
-        return "displayresume";
-    }
-}
-========
 //package com.example.practice1.mailSenderService.model;
 //
 //import org.springframework.stereotype.Controller;
@@ -57,5 +47,3 @@ public class ResumeDisplayController
 //        return "displayresume";
 //    }
 //}
->>>>>>>> 3174df623b76f3e6be593e34217727a0a23c877a:src/main/java/com/example/practice1/mailSenderService/model/ResumeDisplayController.java
->>>>>>> 3174df623b76f3e6be593e34217727a0a23c877a
